8f07dc107a1622b75ffbc17555b2bf00ef8dbb5c
Martin Letis <mletis@google.com>
2021-01-07 22:43:37 +1100

    Simpler loop

--- src/main/java/com/martinletis/contacts/ContactEntryValidator.java
+++ src/main/java/com/martinletis/contacts/ContactEntryValidator.java
@@ -14,14 +14,11 @@ import com.google.api.services.people.v1.PeopleService;
 import com.google.api.services.people.v1.PeopleService.People;
 import com.google.api.services.people.v1.PeopleServiceScopes;
 import com.google.api.services.people.v1.model.Address;
-import com.google.api.services.people.v1.model.GetPeopleResponse;
 import com.google.api.services.people.v1.model.ListConnectionsResponse;
 import com.google.api.services.people.v1.model.Person;
-import com.google.api.services.people.v1.model.PersonResponse;
 import com.google.api.services.people.v1.model.PhoneNumber;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.RateLimiter;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
@@ -30,7 +27,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ContactEntryValidator {
 
@@ -41,7 +37,6 @@ public class ContactEntryValidator {
 
   private static final int MAX_PAGE_SIZE = 500;
   private static final int MAX_REQUESTS_PER_MINUTE = 5;
-  private static final int MAX_RESOURCE_NAMES = 50;
 
   private static final PhoneNumberUtil PHONE_NUMBER_UTIL = PhoneNumberUtil.getInstance();
 
@@ -86,68 +81,39 @@ public class ContactEntryValidator {
             .build()
             .people();
 
+    RateLimiter limiter = RateLimiter.create((double) MAX_REQUESTS_PER_MINUTE / 60);
     String pageToken = null;
     do {
+      limiter.acquire();
       ListConnectionsResponse connectionsResponse =
           people
               .connections()
               .list("people/me")
               .setPageToken(pageToken)
               .setPageSize(MAX_PAGE_SIZE)
+              .setPersonFields("names,phoneNumbers,addresses")
               .execute();
 
-      List<String> resourceNames =
-          connectionsResponse
-              .getConnections()
-              .stream()
-              .map(Person::getResourceName)
-              .collect(Collectors.toList());
+      for (Person person : connectionsResponse.getConnections()) {
+        for (PhoneNumber phoneNumber : safe(person.getPhoneNumbers())) {
+          String number = phoneNumber.getValue();
 
-      RateLimiter limiter = RateLimiter.create((double) MAX_REQUESTS_PER_MINUTE / 60);
-      for (List<String> partition : Lists.partition(resourceNames, MAX_RESOURCE_NAMES)) {
-        limiter.acquire();
+          Preconditions.checkState(number.startsWith("+"), person);
 
-        GetPeopleResponse peopleResponse =
-            people.getBatchGet().setResourceNames(partition).execute();
+          Phonenumber.PhoneNumber proto = PHONE_NUMBER_UTIL.parse(number, null);
+          Preconditions.checkState(PHONE_NUMBER_UTIL.isValidNumber(proto), proto);
 
-        // TODO: filter
-        List<Person> persons =
-            peopleResponse
-                .getResponses()
-                .stream()
-                .map(PersonResponse::getPerson)
-                .collect(Collectors.toList());
+          String formatted =
+              PHONE_NUMBER_UTIL.format(proto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
 
-        for (Person person : persons) {
-          String name =
-              person
-                  .getNames()
-                  .stream()
-                  .filter(n -> n.getMetadata().getPrimary())
-                  .findFirst()
-                  .get()
-                  .getDisplayName();
-
-          for (PhoneNumber phoneNumber : safe(person.getPhoneNumbers())) {
-            String number = phoneNumber.getValue();
-
-            Preconditions.checkState(number.startsWith("+"), name + " - " + number);
-
-            Phonenumber.PhoneNumber proto = PHONE_NUMBER_UTIL.parse(number, null);
-            Preconditions.checkState(PHONE_NUMBER_UTIL.isValidNumber(proto), proto);
-
-            String formatted =
-                PHONE_NUMBER_UTIL.format(proto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
-
-            if (!formatted.equals(number)) {
-              System.out.println(String.format("%s - changing %s to %s", name, number, formatted));
-            }
+          if (!formatted.equals(number)) {
+            System.out.println(String.format("%s - changing %s to %s", person, number, formatted));
           }
+        }
 
-          for (Address address : safe(person.getAddresses())) {
-            if (address.getStreetAddress().contains("\n")) {
-              System.err.println(name);
-            }
+        for (Address address : safe(person.getAddresses())) {
+          if (address.getStreetAddress().contains("\n")) {
+            System.err.println(person);
           }
         }
       }
