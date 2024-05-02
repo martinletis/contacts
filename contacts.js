615fcb4792a40138341bc7389952f659e4b3e241
Martin Letis <mletis@google.com>
2024-05-02 12:53:52 +1000

    Fix initTokenClient() params for compilation

--- contacts.js
+++ contacts.js
@@ -107,8 +107,8 @@ function initAuth() {
     'client_id': '927409904390-9tidmge2ih2brcffsmt41t82knnucss4.apps.googleusercontent.com',
     'scope': 'https://www.googleapis.com/auth/contacts.readonly',
     'callback': tokenResponse => listConnections(tokenResponse.access_token, undefined),
-    prompt: '',
-    enable_granular_consent: false,
+    'prompt': '',
+    'enable_granular_consent': false,
   });
   
   client.requestAccessToken();
